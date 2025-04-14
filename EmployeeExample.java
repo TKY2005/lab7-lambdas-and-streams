9642f693bd7031398fe995cf8f7907e5a5624e4c
TKY2005 <yousoftec@hotmail.com>
2025-04-14 14:55:09 +0300

    Lab7

---
+++ EmployeeExample.java
@@ -0,0 +1,5 @@
+public class EmployeeExample {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ ListExample.java
@@ -0,0 +1,34 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+public class ListExample {
+
+    public static void main(String[] args) {
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("logListFile.txt"))){
+            List<Integer> list = new ArrayList<Integer>();
+            for(int i = 0; i < 100; i++) list.add(new Random().nextInt(1, 100));
+            System.out.println(list);
+            writer.append("Original list :\n");
+            writer.append(list + "\n");
+
+            int threshold = new Random().nextInt(1, 100);
+            writer.append("Selected threshold: " + threshold + "\n");
+            System.out.println("Selected threshold : " + threshold);
+
+            list = list.stream().filter(x -> x < threshold).sorted().toList();
+            // this displays each element using stream operations. but i thought that printing it as a list would be better.
+            // list.stream().filter(x -> x < threshold).sorted().forEach(System.out::println)
+            System.out.println(list);
+            writer.append("Processed list: \n");
+            writer.append(list.toString());
+            writer.close();
+        }
+        catch(IOException ioProblem){
+            System.out.println("Couldn't write to file.");
+            System.out.println(ioProblem.getStackTrace().toString());
+        }
+    }
+}
